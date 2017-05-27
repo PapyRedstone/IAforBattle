017c0f94ee46313e9e637918c8e68eddea4f2d35
PapyRedstone <febvrealex@yahoo.fr>
2017-05-27 23:03:51 +0200

    modifié :         src/main.cpp

--- Client/src/main.cpp
+++ Client/src/main.cpp
@@ -3,19 +3,9 @@
 #include "Game.hpp"
 
 int main() {
-  std::string data;
-  std::cin >> data;
-  Game game("172.17.3.250", 53000, data, sf::Color::Blue);
+  Game game("127.0.0.1", 53000, data, sf::Color::Blue);
 
-  while (getchar() != 'q') {
-    auto map = game.getMap();
-    for (auto& i : map) {
-      for (auto& h : i) {
-        std::cout << h;
-      }
-      std::cout << std::endl;
-    }
-  }
+
 
   return 0;
 }
